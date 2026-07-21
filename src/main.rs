fcd0ab507ff4d1fad2f8b789ada9942fa3455f5a
LTWaffles86 <hl1254@scarletmail.rutgers.edu>
2026-07-21 18:41:35 -0400

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,12 @@
-use std::env;
+use std::{env, str::FromStr};
+
+use nom::{
+    IResult, Parser,
+    bytes::complete::take,
+    character::complete::{char, digit1},
+    sequence::delimited,
+};
+use serde_json::Number;
 
 // Available if you need it!
 // use serde_bencode
@@ -6,16 +14,26 @@ use std::env;
 #[allow(dead_code)]
 fn decode_bencoded_value(encoded_value: &str) -> serde_json::Value {
     // If encoded_value starts with a digit, it's a number
-    if encoded_value.chars().next().unwrap().is_ascii_digit() {
-        // Example: "5:hello" -> "hello"
-        let colon_index = encoded_value.find(':').unwrap();
-        let number_string = &encoded_value[..colon_index];
-        let number = number_string.parse::<usize>().unwrap();
-        let string = &encoded_value[colon_index + 1..colon_index + 1 + number];
+    if let Ok((_, string)) = parse_string(encoded_value) {
         serde_json::Value::String(string.to_string())
+    } else if let Ok((_, number)) = parse_ben_integer(encoded_value) {
+        serde_json::Value::Number(number.into())
     } else {
         panic!("Unhandled encoded value: {}", encoded_value)
     }
+}
+
+fn parse_string(s: &str) -> IResult<&str, &str> {
+    let (rest, len) = parse_number::<usize>(s)?;
+    (char(':'), take(len)).map(|(_, s)| s).parse(rest)
+}
+
+fn parse_number<I: FromStr>(s: &str) -> IResult<&str, I> {
+    digit1.map_res(str::parse::<I>).parse(s)
+}
+
+fn parse_ben_integer(s: &str) -> IResult<&str, i64> {
+    delimited(char('i'), parse_number::<i64>, char('e')).parse(s)
 }
 
 // Usage: your_program.sh decode "<encoded_value>"
